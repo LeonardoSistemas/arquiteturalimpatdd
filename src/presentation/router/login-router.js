7363aa80a30dfa5319c5d280ed2336147c105186
Leonardo Treviso <leonardosistemas@outlook.com>
2021-02-13 14:38:43 -0300

    ensure LoginRouter calls AuthUseCase with correct pparams

--- src/presentation/router/login-router.js
+++ src/presentation/router/login-router.js
@@ -1,5 +1,9 @@
 const HttpResponse = require('../helpers/http-response')
+
 module.exports = class LoginRouter {
+    constructor(authUseCase){
+        this.authUseCase = authUseCase
+    }
     route(httpRequest) {
         if (!httpRequest || !httpRequest.body) {
             return HttpResponse.serverError()
@@ -13,6 +17,7 @@ module.exports = class LoginRouter {
             return HttpResponse.badRequest('password')
 
         }
+        this.authUseCase.auth(email, password)
     }
 }
 
