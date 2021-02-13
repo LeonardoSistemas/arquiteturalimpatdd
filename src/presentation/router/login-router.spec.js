051613f1ee7db0cc690127256ccc5f0c0615c9f6
Leonardo Treviso <leonardosistemas@outlook.com>
2021-02-12 23:14:15 -0300

    ensure LoginRouter returns 400 if no password is provided

--- src/presentation/router/login-router.spec.js
+++ src/presentation/router/login-router.spec.js
@@ -2,7 +2,7 @@ const { TestWatcher } = require("jest");
 
 class LoginRouter {
     route(httpRequest) {
-        if (!httpRequest.body.email) {
+        if (!httpRequest.body.email || !httpRequest.body.password) {
             return {
                 statusCode: 400
             }
@@ -12,14 +12,22 @@ class LoginRouter {
 
 describe('Login router', () => {
     test('Should return 400 if no email is provided', () => {
-
         const sut = new LoginRouter()
         const httpRequest = {
-
             body: {
                 password: "any_password"
             }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
 
+    test('Should return 400 if no password is provided', () => {
+        const sut = new LoginRouter()
+        const httpRequest = {
+            body: {
+                email: "any_email@mail.com"
+            }
         }
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
