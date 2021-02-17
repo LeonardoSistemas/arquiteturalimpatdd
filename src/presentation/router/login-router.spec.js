6c8ceb9b7360ff68449c3b399072bd0b8a957ec7
Leonardo Treviso <leonardosistemas@outlook.com>
2021-02-16 23:21:36 -0300

    ensure LoginRouter return 500 if emailValidator trhows

--- src/presentation/router/login-router.spec.js
+++ src/presentation/router/login-router.spec.js
@@ -29,6 +29,16 @@ const makeEmailValidator = () =>{
     return emailValidatorSpy
 }
 
+const makeEmailValidatorWithError = () =>{
+    class EmailValidator{
+        isValid(){
+            throw new Error()
+        }
+    }
+
+    return new EmailValidator()
+}
+
 const makeAuthUseCase = ()=>{
 
     class AuthUseCaseSpy{
@@ -213,5 +223,19 @@ describe('Login router', () => {
         expect(httpResponse.statusCode).toBe(500)
         expect(httpResponse.body).toEqual(new ServerError)
     })
-
+    
+    test('Should return 500 if EmailValidator throws', async () => {                
+        
+        const authUseCaseSpy = makeAuthUseCase()
+        const emailValidatorSpy = makeEmailValidatorWithError()
+        const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy)
+        const httpRequest = {
+            body: {
+                email: "any_email@mail.com",
+                password: "any_password"
+            }
+        }
+        const httpResponse = await sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
+    })
 })
